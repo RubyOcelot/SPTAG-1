eb729ef31f45bee48c81c09e416e5e3bfa4b863b
RubyOcelot(loliw) <ruby_ocelot@outlook.com>
2022-04-20 00:31:05 +0800

    add KeyValueIO and RocksDBIO

---
+++ AnnService/inc/IVF/interfaces/KeyValueIO.h
@@ -0,0 +1,33 @@
+#ifndef SPTAGLIB_KEYVALUEIO_H
+#define SPTAGLIB_KEYVALUEIO_H
+
+#include "inc/Core/Common.h"
+#include "inc/IVF/CommonUse.h"
+
+
+namespace IVF {
+    class KeyValueIO {
+    public:
+        virtual ~KeyValueIO() {}
+
+        virtual bool Initialize(const char *filePath) = 0;
+
+        virtual void ShutDown() = 0;
+
+        virtual SPTAG::ErrorCode Get(const std::string &key, std::string *value) = 0;
+
+        virtual SPTAG::ErrorCode Get(HeadIDType key, std::string *value) = 0;
+
+        virtual SPTAG::ErrorCode Put(const std::string &key, const std::string &value) = 0;
+
+        virtual SPTAG::ErrorCode Put(HeadIDType key, const std::string &value) = 0;
+
+        virtual SPTAG::ErrorCode Merge(HeadIDType key, const std::string &value) = 0;
+
+        virtual SPTAG::ErrorCode Delete(HeadIDType key) = 0;
+
+        virtual void ForceCompaction()=0;
+    };
+}
+
+#endif //SPTAGLIB_KEYVALUEIO_H
---
+++ AnnService/inc/IVF/utils/RocksDBIO.h
@@ -0,0 +1,128 @@
+#ifndef SPTAGLIB_ROCKSDBIO_H
+#define SPTAGLIB_ROCKSDBIO_H
+
+#include <string>
+#include "inc/Core/Common.h"
+
+#include "inc/Helper/KeyValueIO.h"
+#include "inc/Helper/StringConvert.h"
+#include "rocksdb/db.h"
+#include "rocksdb/slice.h"
+#include "rocksdb/options.h"
+#include "rocksdb/merge_operator.h"
+#include "inc/IVF/interfaces/KeyValueIO.h"
+
+using namespace SPTAG;
+
+namespace IVF {
+
+    class RocksDBIO: public KeyValueIO{
+    public:
+        RocksDBIO() = default;
+
+        ~RocksDBIO() override  {
+            db->Close();
+            DestroyDB(dbPath, dbOptions);
+            delete db;
+        }
+
+        bool Initialize(const char *filePath) override  {
+            dbPath = std::string(filePath);
+            dbOptions.create_if_missing = true;
+            dbOptions.IncreaseParallelism();
+            dbOptions.OptimizeLevelStyleCompaction();
+            dbOptions.merge_operator.reset(new AppendMergeOperator);
+
+            auto s = rocksdb::DB::Open(dbOptions, dbPath, &db);
+            LOG(Helper::LogLevel::LL_Info, "Inverted Index: New Rocksdb: %s\n", filePath);
+            return s == rocksdb::Status::OK();
+        }
+
+        void ShutDown() override  {
+            db->Close();
+            DestroyDB(dbPath, dbOptions);
+            delete db;
+        }
+
+        ErrorCode Get(const std::string &key, std::string *value) override  {
+            auto s = db->Get(rocksdb::ReadOptions(), key, value);
+            if (s == rocksdb::Status::OK()) {
+                return ErrorCode::Success;
+            } else {
+                return ErrorCode::Fail;
+            }
+        }
+
+        ErrorCode Get(SizeType key, std::string *value) override  {
+            return Get(Helper::Convert::Serialize<SizeType>(&key), value);
+        }
+
+        ErrorCode Put(const std::string &key, const std::string &value) override {
+            auto s = db->Put(rocksdb::WriteOptions(), key, value);
+            if (s == rocksdb::Status::OK()) {
+                return ErrorCode::Success;
+            } else {
+                return ErrorCode::Fail;
+            }
+        }
+
+        ErrorCode Put(SizeType key, const std::string &value) override  {
+            return Put(Helper::Convert::Serialize<SizeType>(&key), value);
+        }
+
+        class AppendMergeOperator : public rocksdb::AssociativeMergeOperator {
+        public:
+            bool Merge(const rocksdb::Slice &key, const rocksdb::Slice *existing_value,
+                       const rocksdb::Slice &value, std::string *new_value,
+                       rocksdb::Logger *logger) const override {
+                std::string newPosting;
+                if (existing_value) {
+                    newPosting += (*existing_value).ToString();
+                    newPosting += value.ToString();
+                } else {
+                    newPosting += value.ToString();
+                }
+                *new_value = newPosting;
+                return true;
+            }
+
+            const char *Name() const override {
+                return "AppendMergeOperator";
+            }
+        };
+
+        ErrorCode Merge(SizeType key, const std::string &value) override {
+            if (value.empty()) {
+                LOG(Helper::LogLevel::LL_Error, "Error! empty append posting!\n");
+            }
+            auto s = db->Merge(rocksdb::WriteOptions(), Helper::Convert::Serialize<int>(&key, 1), value);
+            if (s == rocksdb::Status::OK()) {
+                return ErrorCode::Success;
+            } else {
+                return ErrorCode::Fail;
+            }
+        }
+
+        ErrorCode Delete(SizeType key) override  {
+            auto s = db->Delete(rocksdb::WriteOptions(), Helper::Convert::Serialize<int>(&key, 1));
+            if (s == rocksdb::Status::OK()) {
+                return ErrorCode::Success;
+            } else {
+                return ErrorCode::Fail;
+            }
+        }
+
+        void ForceCompaction() override {
+            LOG(Helper::LogLevel::LL_Info, "Start Compaction\n");
+            db->CompactRange(rocksdb::CompactRangeOptions(), nullptr, nullptr);
+            LOG(Helper::LogLevel::LL_Info, "Finish Compaction\n");
+        }
+
+    private:
+        std::string dbPath;
+        rocksdb::DB *db{};
+        rocksdb::Options dbOptions;
+    };
+}
+
+#endif //SPTAGLIB_ROCKSDBIO_H
